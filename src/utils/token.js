6742b1efa7a8f7f189fc58afad713c082cf5f0e1
cs221069-eng <cs221069@dsu.edu.pk>
2026-04-14 12:10:47 +0500

    Enhance token cookie security settings
    Added secure and sameSite attributes to the token cookie.

--- src/utils/token.js
+++ src/utils/token.js
@@ -14,11 +14,12 @@ const generateToken = (user,res) =>{
         { expiresIn: '7d' }
     );
 
-    res.cookie("token" , token ,{
-        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-        httpOnly: true,
-    });
-
+   res.cookie("token", token, {
+  expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  httpOnly: true,
+  secure: true,        
+  sameSite: "none",    
+});
     return token;
 }
 
